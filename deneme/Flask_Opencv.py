94f9225e9f9638a4dfcf9e994c9974441ef12529
cangerek06 <133831585+cangerek06@users.noreply.github.com>
2023-06-02 16:19:49 +0300

    Update Flask_Opencv.py

--- deneme/Flask_Opencv.py
+++ deneme/Flask_Opencv.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 import face_recognition
 import os
+import numpy as np
 import psycopg2
 import db_operations
 
@@ -33,7 +34,7 @@ besimKayit=[]
 def VideoEkle(videoLink):
     video_conn=psycopg2.connect(host ="localhost",dbname="flask_db",user="postgres",password="1",port=5432)
     video_cur = video_conn.cursor()
-    video_cur.execute("""CREATE TABLE IF NOT EXISTS VideoTable(
+    video_cur.execute("""CREATE TABLE IF NOT EXISTS videotable(
     id SERIAL PRIMARY KEY,
     videoLink VARCHAR(255)
     );""")
@@ -47,7 +48,7 @@ def VideoEkle(videoLink):
 def videoCek(VideoId):
     video_conn=psycopg2.connect(host ="localhost",dbname="flask_db",user="postgres",password="1",port=5432)
     video_cur = video_conn.cursor()
-    sorgu = f"select * from VideoTable where id = {VideoId}"
+    sorgu = f"select * from videotable where id = {VideoId}"
 
     video_cur.execute(sorgu)
     cekilenVeri =video_cur.fetchall()
@@ -83,6 +84,13 @@ def imgExtracter(Id):
     
 
 def KisiTanimla(Id):
+
+    """
+    gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    faces = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))"""
+
     for name in os.listdir(KNOWN_FACES_DIR):
         for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
             image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
@@ -104,29 +112,61 @@ def KisiTanimla(Id):
     for i in range(80,len(os.listdir("data"))):
         filename = f"frame{i}.jpg"
         face_list =[]
+        face_ratio_list =[]
+        print("#########################")
         print(filename)
+        
         print("Dosya var mi :"+str(os.path.exists(f"data/{filename}")))
+        print("*****************")
         image =cv2.imread(f"data/{filename}")
+        imageWidth, imageHeight, imageChannel= image.shape
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+        faces = face_classifier.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
+
         locations = face_recognition.face_locations(image,2)
         encoding = face_recognition.face_encodings(image,locations)
         image =cv2.cvtColor(image,cv2.COLOR_RGB2BGR)
-        for face_encoding, face_location in zip(encoding, locations):
+        for face_encoding, face_location, (x,y,w,h)  in zip(encoding, locations, faces):
             results = face_recognition.compare_faces(known_faces,face_encoding,TOLERANCE)
             match = None
+            face_area =w * h
+            print("İmage Width : "+str(imageWidth))
+            print("İmage Height : "+str(imageHeight))
+            print("***********************")
+            print("Face Width : "+str(w))
+            print("Face Height : "+str(h))
+            print("***********************")
+            print("İmage Area : "+str(imageHeight * imageWidth))
+            print("Face Area : "+str(h * w))
+            print("***********************")
+            FaceRatio = (((h * w) /(imageHeight * imageWidth))*100)
+            print("Ratio of Face to Image : %"+str(((h * w) /(imageHeight * imageWidth))*100))
+
         
             if True in results:
                 match = known_names[results.index(True)]
                 print(f"Match Found : {match}")
                 if(match =="celal"):
                     celalKayit.append((i))
-                    face_list.append("CelalSengor")
+                    face_list.append(("CelalSengor"))
+                    face_ratio_list.append(FaceRatio)
                 if(match=="ali"):
                     aliKayit.append((i))
-                    face_list.append("MehmetAliBirand")
+                    face_list.append(("MehmetAliBirand"))
+                    face_ratio_list.append(FaceRatio)
                 if(match=="besim"):
                     besimKayit.append((i))
-                    face_list.append("BesimTibuk")
-        db_operations.InsertDataToAnalyzePerFrame(host="localhost",dbname="flask_db",user="postgres",password="1",port=5432,RecievedData=face_list,videoId=Id,frameNumber = i)
+                    face_list.append(("BesimTibuk"))
+                    face_ratio_list.append(FaceRatio)
+            print("###################")
+
+            print("facelist : "+str(face_list))
+            print("face_ratio_list : "+str(face_ratio_list))
+        
+        db_operations.DbInitiliazer(host="localhost",dbname="flask_db",user="postgres",password="1",port=5432)
+        db_operations.InsertDataToAnalyzePerFrame(host="localhost",dbname="flask_db",user="postgres",password="1",port=5432,RecievedData1=face_list,RecievedData2=face_ratio_list,videoId=Id,frameNumber = i)
+
 
                     
                 
@@ -156,6 +196,7 @@ def KafaSay():
 
         for (x, y, w, h) in faces:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+            
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         writedPath = "/home/can/Desktop/Calismalar/CanPython/Flask/deneme/static/committed"+str(i)+".png"
         cv2.imwrite(writedPath,img_rgb)
@@ -167,13 +208,20 @@ def KafaSay():
         print("Kafa Sayısı :"+str(i[1]))
 
 
-@app.route('/compare/<person>')
-def CompareTime(person):
-    if(bilgiler ==[]):
+@app.route('/compare/<int:id>/<person>')
+def CompareTime(person,id):
+    conn=psycopg2.connect(host="localhost",dbname="flask_db",user="postgres",password="1",port=5432)
+    cursor =conn.cursor()
+    cursor.execute(f"SELECT * FROM analyzeperframe WHERE id={id}")
+    data = cursor.fetchall()
+    print("Data :"+str(data))
+    if(data ==[]):
         return render_template("compareTime.html",person=person,BilgiDurumu=False)
     else:
+
         onlyPersonFrameList = db_operations.faceComparisonbyFrames("localhost","flask_db","postgres","1",5432,person)
-        return render_template("compareTime.html",person=person,BilgiDurumu=True,onlyPersonFrameList=onlyPersonFrameList)
+        ratio_list = db_operations.GiveFaceRatio("localhost","flask_db","postgres","1",5432,onlyPersonFrameList,id)
+        return render_template("compareTime.html",person=person,BilgiDurumu=True,onlyPersonFrameList=onlyPersonFrameList,ratio_list=ratio_list)
 
 
 @app.route('/info')
